4bf6ed20e9cc5894bf96c2be3fd2571cf0782477
Evelin <evelinrpaumgertner@hotmail.com>
2025-02-09 18:43:22 -0300

    implement Chainlink VRF and price feed

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,39 +1,87 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery {
+
+contract Lottery is Ownable, VRFConsumerBase{
 
     address payable[] public players;
     uint public usdEnterFee;
+    // vrf variables
+    uint256 public fee;
+    bytes32 public keyhash;
+    uint256 public randomResult;
 
     AggregatorV3Interface internal ethUsdPriceFeed;
+    //after lottery
+    address payable public recentWinner;
 
-    event PriceUpdated(int256 price);
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
 
-    constructor(address _ethUsdPriceFeed) {
+    LOTTERY_STATE public lottery_state;
+
+    constructor(
+        address _priceFeedAddress, 
+        address _vrfCoordinator,
+        address _linkToken,
+        uint256 _fee,
+        bytes32 _keyhash
+        ) public VRFConsumerBase(_vrfCoordinator, _linkToken){
         usdEnterFee = 50 * (10 ** 18);
-        ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN, "Lottery is not open!");
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(payable(msg.sender));
-
-        // Emitir el precio cuando un jugador entre
-        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
-        emit PriceUpdated(price);
     }
 
     function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
-        require(price > 0, "Invalid price data from price feed");
-        uint256 adjustedPrice = uint256(price) * 10 ** 10;
-        uint256 costToEnter = (usdEnterFee * 10 ** 18) / adjustedPrice;
+        uint256 adjustedPrice = uint256(price) * 10**10;
+        uint256 costToEnter = (usdEnterFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+             "Lottery is alrerady open!!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;    
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner{
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "loterry is not open");
+        require(randomness > 0, "random-not-found");
+        uint256 indexOfWinner = randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        //PAY TO THE WINNER
+        recentWinner.transfer(address(this).balance);
+        //reset the players array
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = randomResult;
+    }
+
+    function getPriceFeed() public view returns (address) {
+    return address(ethUsdPriceFeed);
+}
+
 }
--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,6 +1,6 @@
 from brownie import Lottery, accounts, config, network, interface
 from web3 import Web3
-import pytest
+
 
 def test_get_entrance_fee():
     account = accounts[0]
@@ -8,7 +8,3 @@ def test_get_entrance_fee():
         config["networks"][network.show_active()]["eth_usd_price_feed"],
         {"from": account, "gas_limit": 4000000, "gas_price": Web3.to_wei('20', 'gwei')}
     )
-    # Escuchar el evento
-    tx = lottery.getEntranceFee()
-    receipt = tx.events['PriceUpdated']
-    print(f"Price: {receipt['price']}")
